1297d48778e41050e827af1d5ccff2c68a731428
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2024-06-02 20:22:57 +0545

    Added another way of reversing the string. For pratice of course.

--- reverse.cpp
+++ reverse.cpp
@@ -10,15 +10,21 @@ int main(){
     cout << "Enter the word that you want to reverse:";
     cin >> word;
 
-    // cout << "First letter of the word:" << word[0] << endl;
-
+    cout << "First letter of the word:" << word[0] << endl;
+    // really stupid way of doing this 
     for(int i = 0; i <  word.length()+1; i++){
         cout << word[word.length()-i] << endl;
 
         finalWord += word[word.length()-i];
-
     }
+    // cout << "The reversed word is:" << finalWord << endl;
+    // may be a better way?
 
+    for(int i = word.length(); i+1 > 0; i--){
+        cout << word[i] << endl;
+
+        finalWord += word[i];
+    }
     cout << "The reversed word is:" << finalWord << endl;
     return 0;
 }
